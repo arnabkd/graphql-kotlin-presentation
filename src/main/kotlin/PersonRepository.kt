30a6331332b40fd07b0ede06cd2f1cea6618b297
Arnab Kumar Datta <arnab.datta@evry.com>
2021-04-09 11:07:50 +0200

    update

--- src/main/kotlin/PersonRepository.kt
+++ src/main/kotlin/PersonRepository.kt
@@ -7,13 +7,15 @@ object PersonRepository {
     val rocket = PetsRepository.findById(1)
     val sherlock = PetsRepository.findById(2)
 
-    val Jessica = Person(3, "Jessica", emptyList())
-    val Kim = Person(2, "Kim", listOf(sherlock))
-    val Arnab = Person(1, "Arnab", listOf(sherlock, rocket))
+    val jessica = Person(3, "Jessica", emptyList())
+    val kim = Person(2, "Kim", listOf(sherlock))
+    val arnab = Person(1, "Arnab", listOf(sherlock, rocket))
 
-    people.add(Arnab)
-    people.add(Jessica)
-    people.add(Kim)
+    addFriends(jessica, arnab)
+
+    people.add(arnab)
+    people.add(jessica)
+    people.add(kim)
   }
 
   fun addPerson(name: String): Person {
@@ -32,7 +34,7 @@ object PersonRepository {
     /*.also {
     println("this is an expensive call to find the person with id $id")
   }*/
-  
+
   fun findByIds(ids: List<Int>) = ids.map { findById(it) }
 }
 
