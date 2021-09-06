456a66453cdfec59cace891113d8a7739de9a72e
Arnab Kumar Datta <arnabkum@gmail.com>
2021-09-06 16:47:31 +0200

    CORS for localhost

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,6 +1,10 @@
 import graphql.ExecutionInput
 import graphql.GraphQL
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CORS
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -11,7 +15,23 @@ import ktor.graphql.graphQL
 fun Application.main() {
   val graphQLExecutor = GraphQL.newGraphQL(schema).build()
 
+
   val server = embeddedServer(Netty, port = 8080) {
+    // CORS for localhost
+    install(CORS) {
+      method(HttpMethod.Options)
+      method(HttpMethod.Put)
+      method(HttpMethod.Delete)
+      method(HttpMethod.Patch)
+      header(HttpHeaders.Authorization)
+      header(HttpHeaders.ContentType)
+      // header("any header") if you want to add any header
+      allowCredentials = true
+      allowNonSimpleContentTypes = true
+      host("localhost:3000", listOf("http", "https"))
+      println("CORS enabled for $hosts")
+    }
+
     routing {
       graphQL("/graphql", schema) { request ->
         Config(
